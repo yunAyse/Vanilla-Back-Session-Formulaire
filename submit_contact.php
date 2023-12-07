8c098223f3d66b5378fecd1e3aecc7ed37ad3a9a
yunAyse <aysepc0@gmail.com>
2023-12-07 16:50:21 +0100

    first progress

---
+++ submit_contact.php
@@ -0,0 +1,35 @@
+<!-- <
+
+
+// PARTIE 7 
+
+// $lastName = $_POST['last-name'];
+// $firstName = $_POST['first-name'];
+// $civilite = $_POST['civilite'];
+
+// echo $civilite .' '. $lastName .' '. $firstName ;
+
+
+
+// OC CODE
+// if(empty($_POST['email']) || filter_var($_POST['email'],FILTER_VALIDATE_EMAIL) && 
+// empty($_POST['message'])){
+//   echo 'Il faut un email et un message pour soumettre le formulaire.';
+
+//   return;
+// } -->
+
+
+<!-- OC CODE -->
+
+<!-- <h1>Message bien recu !</h1>
+
+<div class="card">
+
+<div class="card-body">
+  <h5 class="card-title"></h5>
+  <p class="card-text"><b>Email</b> :<php// echo $_POST['email']; ?></p>
+  <p class="card-text"><b>Message</b> : <php echo $_POST['message']; ?></p>
+</div>
+
+</div> -->
