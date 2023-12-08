5993ff1763c824511ce36c3a93727caff8776f8d
yunAyse <aysepc0@gmail.com>
2023-12-08 09:19:32 +0100

    progress

--- index.php
+++ index.php
@@ -3,49 +3,42 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>PHP - Partie 7</title>
+  <title>PHP - Partie 1</title>
 </head>
 <body>
-  
-  <?php
 
-if (isset($_POST['submit_form'])) {
-
-$lastName = $_POST['last-name'];
-$firstName = $_POST['first-name'];
-$civilite = $_POST['civilite'];
-?> 
-
-<p><?php  echo $civilite .' '. $lastName .' '. $firstName ; ?></p>
-
-
-<?php 
-} else { 
-  ?>
+<?php
+if (isset($_POST['submit-form'])) {
+    $lastName = $_POST['last-name'];
+    $firstName = $_POST['first-name'];
+    $civilite = $_POST['civilite'];
+?>
+    <p><?php echo $civilite .' '. $lastName .' '. $firstName; ?></p>
+<?php
+} else {
+?>
   <form action="" method="POST">
 
     <label for="civilite">Choisir une civilité</label>
     <select name="civilite" id="civilite">
-    <option value="Monsieur">Monsieur</option>
-    <option value="Madame">Madame</option>
+      <option value="Monsieur">Monsieur</option>
+      <option value="Madame">Madame</option>
     </select>
 
     <label for="last-name"></label>
     <input type="text" placeholder="Nom" name="last-name">
 
     <label for="first-name"></label>
-    <input type="text" placeholder="Prenom" name="first-name">
-
+    <input type="text" placeholder="Prénom" name="first-name">
 
     <button type="submit" name="submit-form">Envoyer</button>
   </form>
-
-  <?php 
-
-  }
-  ?>
+<?php
+}
+?>
 </body>
 </html>
+
 
 
 
